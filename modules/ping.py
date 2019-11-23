46116df40a85431f73aad15da13fbf602cee7a34
KokoseiJ <kokoseij@gmail.com>
2019-11-24 03:32:37 +0900

    added ping module

---
+++ modules/ping.py
@@ -0,0 +1,16 @@
+import subprocess
+
+mode = "private"
+
+async def main(client, event):
+    print("ping command has been called")
+    address = event.raw_text[1:].split(' ')[1:]
+    if not address:
+        address = "google.com"
+    process = subprocess.run(["ping"] + address, capture_output = True)
+    avr_ping = process.stdout.decode("utf-8").split("\n")[-2].split("/")[-3]
+
+    replytxt ="`pong!\n\n" +\
+    "average ping time to " + address + " is " + avr_ping + ".`"
+
+    event.reply(replytxt)
