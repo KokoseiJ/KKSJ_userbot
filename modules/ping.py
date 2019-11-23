9e7476af7cbdf71715592b45f284fafa20bdf7a5
KokoseiJ <kokoseij@gmail.com>
2019-11-24 03:51:15 +0900

    fixed ping module again

--- modules/ping.py
+++ modules/ping.py
@@ -4,13 +4,21 @@ mode = "private"
 
 async def main(client, event):
     print("ping command has been called")
-    address = event.raw_text[1:].split(' ')[1:]
-    if not address:
-        address = ["google.com"]
-    process = subprocess.run(["ping"] + address + ["-c 4"], capture_output = True)
-    avr_ping = process.stdout.decode("utf-8").split("\n")[-2].split("/")[-3]
-
-    replytxt ="`pong!\n\n" +\
-    "average ping time to " + address[0] + " is " + avr_ping + ".`"
-
-    event.reply(replytxt)
+    try:
+        address = event.raw_text[1:].split(' ')[1:]
+        if not address:
+            address = ["google.com"]
+        process = subprocess.run(["ping"] + address + ["-c 4"], capture_output = True)
+        avr_ping = process.stdout.decode("utf-8").split("\n")[-2].split("/")[-3]
+    
+        replytxt ="`pong!\n\n" +\
+        "average ping time to " + address[0] + " is " + avr_ping + ".`"
+    
+        print(replytxt)
+    
+        await event.reply(replytxt)
+    except Exception as e:
+        replytxt = "`An exception has been occured.\n\n" +\
+        "stdout:" + process.stdout.decode("utf-8") + "`"
+        print(replytxt)
+        await event.reply(replytxt)
