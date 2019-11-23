82172850527bd655a291e61987289f6567f87a15
KokoseiJ <kokoseij@gmail.com>
2019-11-24 03:41:20 +0900

    fixed ping module

--- modules/ping.py
+++ modules/ping.py
@@ -6,11 +6,11 @@ async def main(client, event):
     print("ping command has been called")
     address = event.raw_text[1:].split(' ')[1:]
     if not address:
-        address = "google.com"
-    process = subprocess.run(["ping"] + address, capture_output = True)
+        address = ["google.com"]
+    process = subprocess.run(["ping"] + address + ["-c 4"], capture_output = True)
     avr_ping = process.stdout.decode("utf-8").split("\n")[-2].split("/")[-3]
 
     replytxt ="`pong!\n\n" +\
-    "average ping time to " + address + " is " + avr_ping + ".`"
+    "average ping time to " + address[0] + " is " + avr_ping + ".`"
 
     event.reply(replytxt)
