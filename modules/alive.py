4fa5da4d8ae04182d1176496369a98d26234d468
KokoseiJ <kokoseij@gmail.com>
2019-11-23 15:54:32 +0900

    modified alive.py to show hostname

--- modules/alive.py
+++ modules/alive.py
@@ -1,13 +1,15 @@
 import sys
 import telethon
+import socket
 mode = "private"
 async def main(client, event):
-	print(".alive has been called")
-	replytxt = "`KKSJ_userbot\n" +\
-	"I am alive, Young man!\n\n" +\
-        "Python Version: " + sys.version.split(' ')[0] + "\n" +\
-	"Telethon Version: " + telethon.__version__ + "\n" +\
-	"Owner: Kokosei J`"
-	print(replytxt)
-	await event.reply(replytxt)
-	return
+    print(".alive has been called")
+    replytxt = "`KKSJ_userbot\n" +\
+    "I am alive, Young man!\n\n" +\
+    "Python Version: " + sys.version.split(' ')[0] + "\n" +\
+    "Telethon Version: " + telethon.__version__ + "\n" +\
+    "Server Hostname: " + socket.gethostname() + "\n" +\
+    "Owner: Kokosei J`"
+    print(replytxt)
+    await event.reply(replytxt)
+    return
