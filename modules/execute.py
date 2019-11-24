d1c04293f7247129142f0ffea0a2fb0d6f770288
KokoseiJ <kokoseij@gmail.com>
2019-11-25 02:43:11 +0900

    added execute module

---
+++ modules/execute.py
@@ -0,0 +1,24 @@
+import subprocess
+
+mode = "private"
+
+async def main(client, event):
+    print("execute command has been called")
+    try:
+        command = event.raw_text[1:].split(' ')[1:]
+        if command:
+            process = subprocess.run(command, capture_output = True)
+            replytxt = "`" + process.stdout.decode("utf+8") + "`"
+
+        else:
+            replytxt = "`Seems like you didn't provide a command`"
+
+        print(replytxt)
+
+        await event.reply(replytxt)
+        
+    except Exception as e:
+        replytxt = "`An exception has been occured.\n\n" +\
+        "stdout:" + process.stdout.decode("utf-8") + ons15"`"
+        print(replytxt)
+        await event.reply(replytxt)
